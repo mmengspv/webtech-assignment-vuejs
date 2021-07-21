74929bdbce5c9857788eb12efa9aa9b6b5bb65e7
mmengspv <supavet.a@ku.th>
2021-07-21 17:08:59 +0700

    init project

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <ledger-form></ledger-form>
+    <ledger-list></ledger-list>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import LedgerList from "../components/LedgerList.vue";
+import LedgerForm from "../components/LedgerForm.vue";
+
+export default {
+  components: {
+    LedgerForm,
+    LedgerList,
+  },
+};
+</script>
